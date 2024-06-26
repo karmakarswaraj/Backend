bdd1f2b073287490f8b490b3d3be0d2188784724
karmakarswaraj <swarajkarmakar5@gmail.com>
2024-06-27 00:56:41 +0530

    Added Database and Handles Api response

--- Chaai_Backend/src/database/indexDb.js
+++ Chaai_Backend/src/database/indexDb.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URL}/${DB_NAME}`
     );
-    console.log(`MONGODB connected Hosted at ${connectionInstance.connection.host}`);
+    console.log(`MONGODB connected....product
+Hosted at ${connectionInstance.connection.host}`);
   } catch (error) {
     console.log("MONGODB failed to connect", error);
     process.exit(1);
---
+++ Chaai_Backend/src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (fn) => {
+    return (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    };
+};
+
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+export default asyncHandler 
